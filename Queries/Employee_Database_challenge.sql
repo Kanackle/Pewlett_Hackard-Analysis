712b464a101ff591e7e40056a0d18be4948012de
Kanackle <Kanak.Somani02@myhunter.cuny.edu>
2022-10-10 22:38:35 -0400

    Created new tables to represent retirement info

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,48 @@
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO ret_employee_title
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+ORDER BY e.emp_no
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM ret_employee_title as rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+--Retrieve number of employees by title who are retiring
+SELECT 
+COUNT(ut.emp_no),ut.title
+INTO retiring_table
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.emp_no) DESC;
+
+--Create a mentorship eligibility table
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+--INTO mentorship_table
+FROM employees as e
+LEFT JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND 
+	(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no 
